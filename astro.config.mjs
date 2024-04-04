21d40fe1dc7131264b5a38682240193262655e69
Lachi Singh <l.singh7@lse.ac.uk>
2024-04-05 00:03:51 +0100

    Fix site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,17 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://lachisingh.com',
-  integrations: [mdx(), sitemap(), tailwind({
-    applyBaseStyles: false
-  }), react()]
+    site: 'https://www.lachisingh.com',
+    integrations: [
+        mdx(),
+        sitemap(),
+        tailwind({
+            applyBaseStyles: false
+        }),
+        react()
+    ]
 });
