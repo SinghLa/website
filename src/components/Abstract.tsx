c3dbcc1f73d0262555ab026144e6e189b4b8d65c
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2026-06-23 21:14:51 +0100

    Upgrade to Tailwind v4 and Astro v6 (#3)
    * Upgrade to Tailwind v4
    
    * Migrate to Astro v6
    
    * Set Node.js version to v22
    
    ---------
    
    Co-authored-by: Lachi Singh <l.singh7@lse.ac.uk>

--- src/components/Abstract.tsx
+++ src/components/Abstract.tsx
@@ -11,7 +11,7 @@ export default function Abstract({ abstract }: Props) {
     if (!expanded) {
         return (
             <>
-                <div className="mt-2 text-sm leading-normal text-justify line-clamp-3 prose prose-dante dark:prose-dark max-w-none">
+                <div className="mt-2 text-sm leading-normal text-justify line-clamp-3 prose prose-dante max-w-none">
                   <Markdown>{abstract}</Markdown>
                 </div>
                 <button className="underline text-sm leading-normal mt-4 italic" onClick={() => setExpanded(true)}>
@@ -23,7 +23,7 @@ export default function Abstract({ abstract }: Props) {
 
     return (
         <>
-            <div className="mt-2 text-sm leading-normal text-justify prose prose-dante dark:prose-dark max-w-none">
+            <div className="mt-2 text-sm leading-normal text-justify prose prose-dante max-w-none">
               <Markdown>{abstract}</Markdown>
             </div>
             <button className="underline text-sm leading-normal mt-4 italic" onClick={() => setExpanded(false)}>
