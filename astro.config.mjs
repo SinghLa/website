514cc0e2796c1059c44b84f62b10a299050dc0ce
Lachi Singh <l.singh7@lse.ac.uk>
2026-06-23 20:56:20 +0100

    Upgrade to Tailwind v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
-
 import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://www.lachisingh.com',
-    integrations: [
-        mdx(),
-        sitemap(),
-        tailwind({
-            applyBaseStyles: false
-        }),
-        react()
-    ]
+    integrations: [mdx(), sitemap(), react()],
+    vite: {
+        plugins: [tailwindcss()]
+    }
 });
